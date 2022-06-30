641e5291fd8f40844c1003216b862d7c84382efe
JulianPogliafito <julianpogliafito@gmail.com>
2022-06-30 21:46:16 +0200

    Proyecto final JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,147 @@
+const cards = document.getElementById("cards")
+const items = document.getElementById("items")
+const footer = document.getElementById("footer")
+const templateCard = document.getElementById("template-card").content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
+const nCarrito = document.querySelector('.nCarrito')
+const fragment = document.createDocumentFragment()
 
-const btn1 = document.querySelector(".btn1")
-const btnIngredientes1 = document.querySelector(".btnIngr1")
+let carrito = {};
 
-// Escuchamos el evento del boton lo quiero! para agregar al carrito 
-btn1.addEventListener(`click`, () =>{
-    const swalWithBootstrapButtons = Swal.mixin({
-        customClass: {
-          confirmButton: 'btn btn-success',
-          cancelButton: 'btn btn-danger'
-        },
-        buttonsStyling: false
-      })
-      
-      swalWithBootstrapButtons.fire({
-        title: 'Excelente! Agregada al carrito',
-        text: "Queres agregar/modificar tu hamburguesa o combo?",
-        icon: 'success',
-        showCancelButton: true,
-        confirmButtonText: 'Si, quiero!',
-        cancelButtonText: 'No, gracias!',
-        reverseButtons: true
-      }).then((result) => {
-        if (result.isConfirmed) {
-          swalWithBootstrapButtons.fire(
-            'Perfecto!',
-            'Selecciona tus preferencias',
-            'success'
-          )
-        } else if (
-          /* Read more about handling dismissals below */
-          result.dismiss === Swal.DismissReason.cancel
-        ) {
-          swalWithBootstrapButtons.fire(
-            'Cancelled',
-            'Your imaginary file is safe :)',
-            'error'
-          )
-        }
-      })
+document.addEventListener('DOMContentLoaded', () =>{
+  fetchData();
+  if(localStorage.getItem('carrito')){
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+    pintarCarrito();
+  }
+})
+cards.addEventListener('click', e => {
+  agregarAlCarrito(e)
 })
 
-// Escuchamos el evento del boton ingredientes
-btnIngredientes1.addEventListener(`click`, () =>{
-  Swal.fire(
-    'Ingredientes FIRE',
-    'Contiene: 2 medallores de carne vacuna, queso de cabra, cebolla caramelizada, lechuga y salsa picante de la casa',
-    'info'
-)
+items.addEventListener('click', e => {
+  btnAccion(e)
 })
 
-class Hamburguesa {
-  constructor(nombre, precio, id, ingredientes, img, cantidad){
-      this.nombre = nombre;
-      this.precio = precio;
-      this.id = id;
-      this.ingredientes = ingredientes;
-      this.img = img;
-      this.cantidad = cantidad;
+const fetchData = async () => {
+  try {
+    const res = await fetch('api.json')
+    const data = await res.json()
+    pintarBurguers(data);
+  } catch (error){
+    console.log(error)
   }
 }
 
+const pintarBurguers = data =>{
+  
+  data.forEach(burguers => {
+    templateCard.querySelector('h5').textContent = `BURGUER ${burguers.nombre}`;
+    templateCard.querySelector('p').textContent =  burguers.precio;
+    templateCard.querySelector('img').setAttribute("src", burguers.imagen);
+    templateCard.querySelector('.btn-dark').dataset.id = burguers.id;
+    const clone = templateCard.cloneNode(true);
+    fragment.appendChild(clone)
+  });
+  cards.appendChild(fragment)
+};
 
-const air = new Hamburguesa (`Air`, 15, 1,`Contiene: Pan, 2 medallones de carne vacuna 150g, queso cheddar, cebolla, panceta y salsa de la casa`, `./img/hamburguesaAir.png`)
-const fire = new Hamburguesa (`Fire`, 20, 2, `Contiene: Pan, 2 medallones de carne vacuna 150g, queso de cabra, cebolla caramelizada, lechuga y salsa picante de la casa`, `./img/hamburguesaFire.png`)
-const ground = new Hamburguesa (`Ground`, 25, 3, `Contiene: Pan, 1 medallon de carne vacuna 200g, queso, cebolla caramelizada, lechuga y salsa barbacoa`, `./img/hamburguesaGround.png`)
-const sea = new Hamburguesa (`Sea`, 18, 4, `Contiene: Pan, 2 medallones de carne vacuna 100g, queso cheddar, pepino, lechuga y salsa de la casa`, `./img/hamburguesaSea.png`)
+const agregarAlCarrito = e =>{
+  
+  if(e.target.classList.contains(`btn-dark`)){
+    setCarrito(e.target.parentElement);
+  } 
+  e.stopPropagation();
+}
 
-const hamburguesas = [sea, air, fire, ground]
+const setCarrito = objeto => {
+  
+  const producto = {
+    id: objeto.querySelector('.btn-dark').dataset.id,
+    nombre: objeto.querySelector('h5').textContent,
+    precio: objeto.querySelector('p').textContent,
+    cantidad: 1
+  }
 
-const divCarrito = document.querySelector(`.contenidoCarrito `)
+  if(carrito.hasOwnProperty(producto.id)){
+    producto.cantidad = carrito[producto.id].cantidad + 1
+  }
+
+  carrito[producto.id] = {...producto}
+  pintarCarrito()
+}
+
+const pintarCarrito = () => {
+  items.innerHTML = ''
+
+  Object.values(carrito).forEach(producto => {
+  templateCarrito.querySelector('th').textContent = producto.id
+  templateCarrito.querySelectorAll('td')[0].textContent = producto.nombre
+  templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+  templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+  templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+  templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
+
+  const clone = templateCarrito.cloneNode(true)
+  fragment.appendChild(clone)
+})
+
+  items.appendChild(fragment)
+
+  pintarFooter();
+
+  localStorage.setItem('carrito', JSON.stringify(carrito))
+  
+}
+
+const pintarFooter = () => {
+  footer.innerHTML = ''
+  if(Object.keys(carrito).length === 0){
+    footer.innerHTML = `
+    <th scope="row" colspan="5">Carrito vacío - Comience a comprar!</th>
+    `
+    return;
+  }
+  const nCantidad = Object.values(carrito).reduce((acc, {cantidad}) => acc + cantidad, 0 )
+  const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio, 0 )
+
+  nCarrito.querySelector('span').textContent = nCantidad
+  templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+  templateFooter.querySelector('span').textContent = nPrecio
+
+  const clone = templateFooter.cloneNode(true)
+  fragment.appendChild(clone)
+  footer.appendChild(fragment)
+
+  const btnVaciar = document.getElementById('vaciar-carrito')
+  btnVaciar.addEventListener('click', () =>{
+    carrito = {}
+    pintarCarrito()
+    nCarrito.querySelector('span').textContent = 0
+  } )
+}
+
+const btnAccion = e => {
+  if(e.target.classList.contains('btn-info')){
+    
+    const producto = carrito[e.target.dataset.id]
+    producto.cantidad ++
+    carrito[e.target.dataset.id] = {...producto}
+    pintarCarrito() 
+  }
+
+  if(e.target.classList.contains('btn-danger')){
+    
+    const producto = carrito[e.target.dataset.id]
+    producto.cantidad--
+    if(producto.cantidad === 0){
+      delete carrito[e.target.dataset.id];
+      nCarrito.querySelector('span').textContent = 0
+    }
+    pintarCarrito()
+  }
+
+  e.stopPropagation();
+}
 
